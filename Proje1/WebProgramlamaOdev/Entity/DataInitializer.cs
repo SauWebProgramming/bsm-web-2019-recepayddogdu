a077949171b4386c5b1b648e08560b27522360b8
recepayddogdu <recepayddogdu@gmail.com>
2019-12-04 02:10:15 +0300

    Update DataInitializer.cs

--- Proje1/WebProgramlamaOdev/Entity/DataInitializer.cs
+++ Proje1/WebProgramlamaOdev/Entity/DataInitializer.cs
@@ -10,7 +10,21 @@ namespace WebProgramlamaOdev.Entity
     {
         protected override void Seed(DataContext context)
         {
+            var kategoriler=new List<Category>()
+            {
+                new Category(){Name = "Kamera",Description = "Kamera Ürünleri"},
+                new Category(){Name = "Bilgisayar",Description = "Bilgisayar Ürünleri"},
+                new Category(){Name = "Elektronik",Description = "Elektronik Ürünleri"},
+                new Category(){Name = "Telefon",Description = "Telefon Ürünleri"},
+                new Category(){Name = "Beyaz Eşya",Description = "Beyaz Eşya Ürünleri"}
+            };
 
+            foreach (var kategori in kategoriler)
+            {
+                context.Categories.Add(kategori);
+            }
+
+            context.SaveChanges();
 
             base.Seed(context);
         }
