949da099f77d610e4c49f0b94cadee1f6bf2b4d0
recepayddogdu <recepayddogdu@gmail.com>
2019-12-04 01:30:30 +0300

    Product.cs class'ına içerik eklendi. Ve Category.cs oluşturuldu.
    Category.cs içerisine içerik eklendi.

--- Proje1/WebProgramlamaOdev/Entity/Product.cs
+++ Proje1/WebProgramlamaOdev/Entity/Product.cs
@@ -8,5 +8,10 @@ namespace WebProgramlamaOdev.Entity
     public class Product
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public bool IsApproved { get; set; }
     }
 }
