2fed159f946e04e0deaf93eac62f2e8b570ae4be
recepayddogdu <recepayddogdu@gmail.com>
2019-12-04 01:43:01 +0300

    EntityFramework Projeye Yüklendi.

--- Proje1/WebProgramlamaOdev/Entity/DataContext.cs
+++ Proje1/WebProgramlamaOdev/Entity/DataContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace WebProgramlamaOdev.Entity
 {
-    public class DataContext
+    public class DataContext:DbContext
     {
     }
 }
