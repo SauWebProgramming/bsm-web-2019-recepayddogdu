0d313bdf6f9550d27d8873f53bc19033ccd3c092
recepayddogdu <recepayddogdu@gmail.com>
2019-12-04 02:03:15 +0300

    Update DataInitializer.cs
    Eğer Model değişikliği varsa db oluştur seçeneği seçildi.

--- Proje1/WebProgramlamaOdev/Entity/DataInitializer.cs
+++ Proje1/WebProgramlamaOdev/Entity/DataInitializer.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace WebProgramlamaOdev.Entity
 {
-    public class DataInitializer
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
     {
+        protected override void Seed(DataContext context)
+        {
+
+
+            base.Seed(context);
+        }
     }
 }
