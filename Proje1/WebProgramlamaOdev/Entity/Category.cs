84b2e83600089668a41951a97d929b25de4b9a47
recepayddogdu <recepayddogdu@gmail.com>
2019-12-04 01:38:08 +0300

    Update Product.cs And Category.cs
    Bire çok ilişki oluşturuldu. Bir kategoride birden çok ürün olabilir(Products) fakat bir ürün yanlızca bir kategoriye ait olabilir.

--- Proje1/WebProgramlamaOdev/Entity/Category.cs
+++ Proje1/WebProgramlamaOdev/Entity/Category.cs
@@ -7,5 +7,10 @@ namespace WebProgramlamaOdev.Entity
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
     }
 }
--- Proje1/WebProgramlamaOdev/Entity/Product.cs
+++ Proje1/WebProgramlamaOdev/Entity/Product.cs
@@ -13,5 +13,8 @@ namespace WebProgramlamaOdev.Entity
         public double Price { get; set; }
         public int Stock { get; set; }
         public bool IsApproved { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
