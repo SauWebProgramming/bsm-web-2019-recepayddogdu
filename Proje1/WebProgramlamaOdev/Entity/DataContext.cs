f9788b98f82591fba9bbd8aabdadab2ad7943222
recepayddogdu <recepayddogdu@gmail.com>
2019-12-04 01:57:37 +0300

    Update DataContext.cs

--- Proje1/WebProgramlamaOdev/Entity/DataContext.cs
+++ Proje1/WebProgramlamaOdev/Entity/DataContext.cs
@@ -8,5 +8,12 @@ namespace WebProgramlamaOdev.Entity
 {
     public class DataContext:DbContext
     {
+
+        public DataContext():base("dataConnection")
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
