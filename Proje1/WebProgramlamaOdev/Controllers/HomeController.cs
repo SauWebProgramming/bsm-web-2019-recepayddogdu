b3fa9577cb5e8829c9a23b1c8e1dcd9fab0c5499
aycabingul <58269571+aycabingul@users.noreply.github.com>
2019-12-06 21:02:44 +0300

    HomeController.cs ve Index.cshtml'de değişiklikler yapıldı.

--- Proje1/WebProgramlamaOdev/Controllers/HomeController.cs
+++ Proje1/WebProgramlamaOdev/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebProgramlamaOdev.Entity;
 
 namespace WebProgramlamaOdev.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext _context = new DataContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            
+            return View(_context.Products.ToList());
+
         }
 
         public ActionResult Details()
