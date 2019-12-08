cbb857aeaa8a2d0f1a713335bc0441bdd4dac7d0
recepayddogdu <recepayddogdu@gmail.com>
2019-12-08 21:28:50 +0300

    Create.cshtml sayfasında güncellemeler yapıldı

--- Proje1/WebProgramlamaOdev/Entity/Category.cs
+++ Proje1/WebProgramlamaOdev/Entity/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,11 @@ namespace WebProgramlamaOdev.Entity
     public class Category
     {
         public int Id { get; set; }
+
+        [DisplayName("Kategori Adı")]
         public string Name { get; set; }
+
+        [DisplayName("Açıklama")]
         public string Description { get; set; }
 
         public List<Product> Products { get; set; }
