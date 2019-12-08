d745080011afd1dc484e3f3dfe950077e6c5e86a
aycabingul <58269571+aycabingul@users.noreply.github.com>
2019-12-09 02:28:52 +0300

    Data annotations kullanıldı.

--- Proje1/WebProgramlamaOdev/Entity/Category.cs
+++ Proje1/WebProgramlamaOdev/Entity/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,9 @@ namespace WebProgramlamaOdev.Entity
     public class Category
     {
         public int Id { get; set; }
-
+        //Data annotations
         [DisplayName("Kategori Adı")]
+        [StringLength(maximumLength:20,ErrorMessage="en fazla 20 karakter girebilirsiniz.")]
         public string Name { get; set; }
 
         [DisplayName("Açıklama")]
