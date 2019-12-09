4372e8b82d1ae1b7052fc637e80cfab08348e2a7
aycabingul <58269571+aycabingul@users.noreply.github.com>
2019-12-09 23:11:07 +0300

    Alışveriş Sepeti'nin güncellemesi.

--- Proje1/WebProgramlamaOdev/Controllers/CartController.cs
+++ Proje1/WebProgramlamaOdev/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebProgramlamaOdev.Models;
 
 namespace WebProgramlamaOdev.Controllers
 {
@@ -13,5 +14,23 @@ namespace WebProgramlamaOdev.Controllers
         {
             return View();
         }
-    }
+
+        public ActionResult AddToCart(int Id)
+        {
+            return View();
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart) Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+
+        }
+}
 }
